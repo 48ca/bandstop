295efb32265a796eeadeb03838ea0fcb189abc5c
James Houghton <jamesthoughton@gmail.com>
2017-03-24 19:40:28 -0400

    Add requirements file; update comments

--- bandstop.py
+++ bandstop.py
@@ -11,9 +11,8 @@ from scipy.io import wavfile
 from scipy import signal
 from scipy.fftpack import fft, fftfreq
 
-plt.ion()
-
-N = 1000
+plt.ion() # Make the plot interactive
+# Allows us to update it in the code
 
 def process(filename):
     fs, snd = wavfile.read(filename)
@@ -22,6 +21,7 @@ def process(filename):
 
     duration = samples/fs
 
+    # The data type used to store the samples
     depth_type = snd.dtype
 
     # Produces interesting results
@@ -57,8 +57,13 @@ def find_outstanding_frequencies(data, points_per_sample, fs):
     # THRESHOLD = 5000000
     diff_data = np.diff(data)
     low_band = points_per_sample//4 # Half of our divided version
-    high_ind = np.argpartition(diff_data[low_band:], -5)[-5:] + low_band
-    low_ind = np.argpartition(diff_data[low_band:], 5)[:5] + low_band
+
+    num_outstanding = 3
+
+    # Find the frequencies of highest difference
+    high_ind = np.argpartition(diff_data[low_band:], -num_outstanding)[-num_outstanding:] + low_band
+    # Find the frequencies of most negative difference
+    low_ind = np.argpartition(-diff_data[low_band:], -num_outstanding)[-num_outstanding:] + low_band
     # np.where(data[high_ind] > THRESHOLD * points_per_sample, high_ind, 0)
     sys.stdout.write("h indices: ")
     print(fs/points_per_sample * (high_ind))
@@ -71,15 +76,18 @@ def find_outstanding_frequencies(data, points_per_sample, fs):
         plt.axvline(x=x, color='g')
 
 def parse(sound_data, depth, samples, fs, duration):
-    FFT_SAMPLE_SIZE = 1000 # 100 milliseconds
+    FFT_SAMPLE_SIZE = 1000 # FFT sample size in milliseconds
     points_per_sample = FFT_SAMPLE_SIZE*fs//1000
-    frequency_data = fftfreq(points_per_sample, FFT_SAMPLE_SIZE/1000)*points_per_sample * FFT_SAMPLE_SIZE/1000
-    frequency_conversion_ratio = fs/points_per_sample
+    # frequency_data = fftfreq(points_per_sample, FFT_SAMPLE_SIZE/1000)*points_per_sample * FFT_SAMPLE_SIZE/1000
+    # frequency_conversion_ratio = fs/points_per_sample
     for i in range(0, samples, points_per_sample):
+        # FFT data
         fft_data = fft(sound_data[i:(i+points_per_sample)])
         real_length = len(fft_data)//2
+        # We only care about the first half; the other half is the same
         real_data = abs(fft_data[:(real_length-1)])
 
+        # Plot the data as a frequency spectrum
         plt.plot(real_data,'r')
         plt.show()
         plt.pause(0.2)
